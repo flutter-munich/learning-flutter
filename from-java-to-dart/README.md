ad3d1db1d1bb3a1b6ff302efd71b5c118ac5dff6
Vince Varga <vince@serialdeveloper.com>
2019-06-15 18:53:25 +0200

    Create a simple Bicycle class, instance, and override its toString method
    So far, we:
    * Created a class, Bicycle
    * Created an instance of this class, bike
    * Used type inference to automatically recognize that the bike is an instance of Bicycle by using the var keyword
    * Created a constructor that uses the this keyword to automatically assign values to instance variables
    * Override toString function to display something more informative than Instance of X
    * Use fat arrow => notation for shortening the function declaration
    * Use string interpolation to insert a variable's value into a string by using the $ sign
    
    This is more or less I remember, at least :D

--- from-java-to-dart/README.md
+++ from-java-to-dart/README.md
@@ -5,3 +5,33 @@
 * Dart is the programming language for Flutter.
 * You can run the examples in [DartPad](https://dartpad.dartlang.org/) or in an IDE (e.g. [Visual Studio Code](https://marketplace.visualstudio.com/items?itemName=Dart-Code.dart-code))
 
+## Create a simple Dart class
+
+```dart
+
+class Bicycle {
+  // Instance variables.
+  // Notice: no private keyword
+  int cadence;
+  int speed;
+  int gear;
+  // Constructor has no body, and that's OK in Dart
+  Bicycle(this.cadence, this.speed, this.gear);
+}
+```
+
+In Dart,
+
+* You can define code outside of classes (unlike Java, where everything is a class).
+* All identifiers are public by default.
+* There's no `public`, `protected`, `private` keywords.
+* Indent with 2 spaces.
+* `dartfmt` autoformats your code.
+* Constructors don't need a body.
+* Using `this` in a constructor's parameter list is a handy shortcut for assigning values to instance variables.
+* `new` is optional.
+* If you know that a variable's value won't change, you can use `final` instead of `var`.
+* Analyzer infers that `var bike = Bicycle(...)` defines a Bicycle instance.
+* Use `@override` to explicitly say that you **want to override** smoething. If it's not an overrideable method, the analyzer complains.
+* Strings can be specified with single and double quotes
+* String interpolation: `'$variable'`
--- from-java-to-dart/main.dart
+++ from-java-to-dart/main.dart
@@ -1,3 +1,49 @@
+
+
+// If you need access to command line arguments, write:
+// main(List<String> args)
 main() {
-  print('Hello');
+  // Type inference: nalyzer infers that var bike defines a Bicycle instance.
+  var bike = Bicycle(2, 0, 0);
+  // Prints:
+  // Instance of 'Bicycle'
+  print(bike);
+}
+
+class Bicycle {
+  // Instance variables
+
+  /// Cadence is the number of revolutions of the crank per minute.
+  ///
+  /// This is the rate at which a cyclist is pedalling/turning the pedals.
+  /// Cadence is directly proportional to wheel speed, but is a distinct
+  /// measurement and changes with gearing—which determines the ratio of
+  /// crank rpm to wheel rpm.
+  int cadence;
+
+  /// Speed of the bike.
+  ///
+  /// For a bicycle to travel at the same speed, using a lower gear
+  /// (larger mechanical advantage) requires the rider to pedal at a
+  /// faster cadence, but with less force. 
+  int speed;
+
+  /// Gearing is the aspect of a bicycle drivetrain that determines the
+  /// relation between the cadence, the rate at which the rider pedals, 
+  ///and the rate at which the drive wheel turns.
+  int gear;
+
+  // Constructor has no body.
+  // Using this in a constructor's parameter list is a handy shortcut
+  // for assigning values to instance variables.
+  Bicycle(this.cadence, this.speed, this.gear);
+
+
+  // The @override annotation tells the analyzer that you are intentionally
+  // overriding a member. The analyzer raises an error if you've failed to
+  // perform the override properly.
+  @override
+  // Fat arrow notation => can be used to shorten simple functions
+  // String interpolation '$variable'
+  String toString() => 'Bicycle: $speed mph';
 }
